6f0a9cc307b2838ec6d1d9f383be9bfe245f76a7
Tuan Kiet <nguyentuankiet309203@gmail.com>
2024-03-24 10:30:06 +0700

    Add slide ability

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,15 +11,25 @@ public class Player : MonoBehaviour
     private float jumpSpeed;
     private Rigidbody2D rb;
     private bool playerUnlock;
-    private bool canDoubleJump; 
+    private bool canDoubleJump;
+    [Header("Slide Info")]
+    [SerializeField] private float slideTimer = 0f;
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float slideCooldown = 0f;
+    private float slideCooldownTimer;
+    private bool isSlide;
+    private float slideTimerCounter = 0f;
     // ======================================================================
     [Header("Collision Info")]
     [SerializeField] private float groundCheckDistance = 0f;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private Transform wallCheck;
     [SerializeField] private Vector2 wallCheckSize;
+    [SerializeField] private float ceilingCheckDistance;
     private bool isFacingWall;
     private bool isGrounded;
+    private bool isUnderCeiling;
+
 
     // ======================================================================START===============================================================
     void Start()
@@ -34,46 +44,76 @@ public class Player : MonoBehaviour
     // ======================================================================UPDATE===============================================================
     void Update()
     {
-        if(isGrounded)
-        {
-            canDoubleJump = true;
-        }
+        
         AnimatorController();
         CheckCollision();
         CheckInput();
+        CheckForSlide();
+        
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+        
         if (playerUnlock && !isFacingWall)
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            MoveFunction();
     }
-
-
-
-
-    // ======================================================================OTHER FUNCTION===============================================================
-    private void AnimatorController()
-    {
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetFloat("xVelocity",rb.velocity.x);
-        anim.SetFloat("yVelocity", rb.velocity.y);
-        anim.SetBool("canDoubleJump", canDoubleJump);
-    }
-    private void CheckCollision()
-    {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
-        isFacingWall = Physics2D.BoxCast(wallCheck.position, wallCheckSize,0,Vector2.zero,whatIsGround);
-    }
-
     private void CheckInput()
     {
         if (Input.GetButtonDown("Fire2"))
             playerUnlock = true;
-        if (Input.GetButtonDown("Jump") )
+        if (Input.GetButtonDown("Jump"))
         {
             JumpFunction();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SlideTimerFunction();
+        }
+    }
+    private void CheckForSlide()
+    {
+        slideTimerCounter -= Time.deltaTime;
+        slideCooldownTimer -= Time.deltaTime;
+        if (slideTimerCounter < 0f && !isUnderCeiling)
+        {
+            isSlide = false;
+        }
+    }
+
+    private void MoveFunction()
+    {
+        if (isFacingWall)
+            return;
+        if (isSlide )
+        {
+            rb.velocity = new Vector2(slideSpeed, rb.velocity.y);
+            
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+    }
+
+    
+
+    private void SlideTimerFunction()
+    {
+        if (rb.velocity!= Vector2.zero && slideCooldownTimer < 0 && isGrounded)
+        {
+            isSlide = true;
+            slideTimerCounter = slideTimer;
+            slideCooldownTimer = slideCooldown;
         }
     }
 
     private void JumpFunction()
     {
+        if(isSlide && isUnderCeiling)
+        {
+            return;
+        }
         if (isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
@@ -83,13 +123,34 @@ public class Player : MonoBehaviour
             jumpSpeed = doubleJumpSpeed;
             canDoubleJump = false;
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-            jumpSpeed = defaultJumpSpeed;        
+            jumpSpeed = defaultJumpSpeed;
         }
     }
+
+
+    // ======================================================================OTHER FUNCTION===============================================================
+    private void AnimatorController()
+    {
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("xVelocity", rb.velocity.x);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("canDoubleJump", canDoubleJump);
+        anim.SetBool("isSlide", isSlide);
+    }
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isFacingWall = Physics2D.BoxCast(wallCheck.position, wallCheckSize, 0, Vector2.zero, whatIsGround);
+        isUnderCeiling = Physics2D.Raycast(transform.position,Vector2.up, ceilingCheckDistance, whatIsGround);
+    }
+
+
+    // ======================================================================GIZMOS===============================================================
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
-        Gizmos.DrawWireCube(wallCheck.position,wallCheckSize);
+        Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
+        Gizmos.DrawLine(transform.position,new Vector2(transform.position.x,transform.position.y + ceilingCheckDistance));
     }
 }
